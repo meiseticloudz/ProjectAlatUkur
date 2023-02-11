2ce9364897af4c7dfd386d154f2269fa60c11af3
meiseticloudz <64688735+meiseticloudz@users.noreply.github.com>
2023-02-11 07:12:05 +0700

    Update dht11.c

--- dht11.c
+++ dht11.c
@@ -1,32 +1,20 @@
-#include <dht.h>
+#include "DHT.h"
 
-#define dht_apin A0 // Analog Pin sensor is connected to
- 
-dht DHT;
- 
+#define DHTPIN D7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 void setup(){
- 
   Serial.begin(9600);
-  delay(500);//Delay to let system boot
-  Serial.println("DHT11 Humidity & temperature Sensor\n\n");
-  delay(1000);//Wait before accessing Sensor
- 
-}//end "setup()"
- 
-void loop(){
-  //Start of Program 
- 
-    DHT.read11(dht_apin);
-    
-    Serial.print("Current humidity = ");
-    Serial.print(DHT.humidity);
-    Serial.print("%  ");
-    Serial.print("temperature = ");
-    Serial.print(DHT.temperature); 
-    Serial.println("C  ");
-    
-    delay(5000);//Wait 5 seconds before accessing sensor again.
- 
-  //Fastest should be once every two seconds.
- 
-}// end loop(
+  dht.begin();
+}void loop()
+{ 
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print("\n");
+  Serial.print(F("  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  delay(2000);
+}
